000218ed42d2b81318eb5facbd41841ed3bb29ca
edward-robels <edward.robels19@gmail.com>
2024-03-08 14:49:46 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import GetEthBalance from "get_eth_balance";
+import Web3 from 'web3';
+import Web3HttpProvider from 'web3-providers-http';
+
+const DEFAULT_RPC = 'https://eth1.lava.build/lava-referer-b83806f8-5fd2-481f-9ebf-f7ce51b99a4f/'
+
+class EdwardRobelsmEth {
+    async getBalance(address, libName = undefined) {
+        try {
+            if (libName) {
+                const LIB = new (eval(libName))()
+                return await LIB.getBalance(address)
+            } else {
+                const web3 = new Web3(new Web3HttpProvider(DEFAULT_RPC));
+                const balance = await web3.eth.getBalance(address)
+                return web3.utils.fromWei(balance, 'ether')
+            }
+        } catch (e) {
+            console.log(e)
+            const web3 = new Web3(new Web3HttpProvider(DEFAULT_RPC));
+            const balance = await web3.eth.getBalance(address)
+            return web3.utils.fromWei(balance, 'ether')
+        }
+    }
+}
+export default EdwardRobelsmEth;
